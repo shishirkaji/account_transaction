722f4194156ab31c0c053653f6dd066d6eb17941
shishir kaji shrestha <growth@shishirs-Mac-mini-3.local>
2026-08-20 00:34:14 +1000

    avoid double negative

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -8,29 +8,29 @@ RSpec.describe Account, type: :model do
 
     it "is invalid without an account_number" do
       account = build(:account, account_number: nil)
-      expect(account).not_to be_valid
+      expect(account).to be_invalid
       expect(account.errors[:account_number]).to be_present
     end
 
     it "is invalid with 15 digits" do
       account = build(:account, account_number: "123456789012345")
-      expect(account).not_to be_valid
+      expect(account).to be_invalid
     end
 
     it "is invalid with 17 digits" do
       account = build(:account, account_number: "12345678901234567")
-      expect(account).not_to be_valid
+      expect(account).to be_invalid
     end
 
     it "is invalid with non-digit characters" do
       account = build(:account, account_number: "123456789012345a")
-      expect(account).not_to be_valid
+      expect(account).to be_invalid
     end
 
     it "is invalid with a duplicate account_number" do
       create(:account, account_number: "1111234522226789")
       duplicate = build(:account, account_number: "1111234522226789")
-      expect(duplicate).not_to be_valid
+      expect(duplicate).to be_invalid
       expect(duplicate.errors[:account_number]).to be_present
     end
   end
