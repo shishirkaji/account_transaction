3380aecd9c56dfcfa6e13282c09123e502811967
shishir kaji shrestha <growth@shishirs-Mac-mini-3.local>
2026-08-20 01:25:00 +1000

    ticket 8: pin parse_amount_to_cents with full stress matrix spec

--- spec/models/transaction_file_spec.rb
+++ spec/models/transaction_file_spec.rb
@@ -1,6 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe TransactionFile, type: :model do
+  describe ".parse_amount_to_cents" do
+    valid_cases = {
+      "500.00" => 50000,
+      "500" => 50000,
+      "0" => 0,
+      "0.00" => 0,
+      "0.5" => 50,
+      "0.05" => 5,
+      "500.5" => 50050,
+      "500.05" => 50005,
+      "1000000.99" => 100000099,
+      "99999999999999999999.99" => 9999999999999999999999,
+      "500.0" => 50000
+    }
+
+    valid_cases.each do |input, expected|
+      it "parses #{input.inspect} to #{expected} cents" do
+        expect(TransactionFile.send(:parse_amount_to_cents, input)).to eq(expected)
+      end
+    end
+
+    invalid_inputs = %w[500.000 abc 12a3 -500 "" 500. .5 " 500" "500 " 1,000 "5 00" 5.0.0 +500]
+
+    invalid_inputs.each do |input|
+      it "rejects #{input.inspect}" do
+        expect(TransactionFile.send(:parse_amount_to_cents, input)).to be_nil
+      end
+    end
+  end
+
   describe ".create_with_transactions" do
     it "creates a valid file with ordered pending transactions from a valid CSV" do
       csv = "1111234522226789,1212343433335665,500.00\n3212343433335755,2222123433331212,1000.00\n"
